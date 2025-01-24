9ac18f050e31d8451a9cf4a86e1238ba329180ff
Guddu Kumar <73424882+guddu1cse@users.noreply.github.com>
2025-01-25 00:25:36 +0530

    update

--- fs-promise.js
+++ fs-promise.js
@@ -14,16 +14,57 @@ function createDir(){
 
 function createFile(filename){
     return new Promise((resolve , reject)=>{
-        fs.writeFile("./output/" +filename +".txt" , "this is file "+filename+".txt" , (err)=>{
+        fs.writeFile("./output/file" +filename +".txt" , "this is file "+filename+".txt" , (err)=>{
             if(err){
                 reject(err);
                 return ;
             }
-            resolve(filename + " is created");
+            resolve(`file${filename}.txt`);
         } );
     });
 }
 
-function unlinkFile(){
-    
+function unlinkFile(filename){
+    return new Promise((resolve , reject)=>{
+        fs.unlink("./output/" + filename , (err)=>{
+            if(err){
+                reject(err);
+                return ;
+            }
+            resolve(filename);
+        });
+    });
 }
+
+function unlinkDir(filename){
+    return new Promise((resolve , reject)=>{
+        fs.rm(filename , {recursive : true} , (err)=>{
+            if(err){
+                reject(err);
+                return ;
+            }
+            resolve(filename  +" is deleted");
+        });
+    });
+}
+
+createDir().then(()=>{
+    let promises = [];
+    for(let y=1 ; y<=10 ; y++){
+        promises.push(createFile(y));
+    }
+    return Promise.all(promises);
+})
+.then((res)=>{
+    let promises = []; 
+    res.forEach((val)=>{
+        promises.push(unlinkFile(val));
+    });
+    return Promise.all(promises);
+})
+.then((res)=>{
+    console.log(res.join(",") + " all deleted");
+    return unlinkDir("./output");
+})
+.then(console.log)
+.catch(console.log);
